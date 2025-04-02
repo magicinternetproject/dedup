2a0d25967802d6440752bdc6c48b5f9b2fcd7e46
Shae Erisson <shae@scannedinavian.com>
2025-04-02 16:11:46 -0400

    the magic is working

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,58 @@
+use std::{
+    collections::VecDeque,
+    io::{self, Write},
+    path::{Path, PathBuf},
+};
 use tokio::fs::*;
 use tokio_uring::fs::{DirBuilder, File};
 
-#[tokio::main]
-async fn main() {
-    // list files in "this" dir
-    let mut dirs = read_dir(".").await.unwrap();
-    // println!("{:?}", dirs);
-    // queue dirs
-    while let Some(f) = dirs.next_entry().await.unwrap() {
-        // check for directory, async
-        println!("{:?}", f.path());
+fn main() {
+    let mut pd: VecDeque<PathBuf> = VecDeque::new();
+    pd.push_back(".".into());
+    tokio_uring::start(real_main(&mut pd));
+}
+async fn real_main(pd: &mut VecDeque<PathBuf>) {
+    while let Some(path) = pd.pop_front() {
+	let mut dirs = read_dir(path).await.unwrap();
+	while let Some(f) = dirs.next_entry().await.unwrap() {
+	    if let Ok(ftype) = f.file_type().await {
+		if ftype.is_file() {
+		    tokio_uring::spawn(printafile(f.path())).await.unwrap();
+		} else if ftype.is_dir() {
+		    pd.push_back(f.path());
+		}
+	    }
+	}
     }
 }
 
-// fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     tokio_uring::start(async {
-//	// Open a file
-//	let file = File::open("Cargo.toml").await?;
+async fn printafile(f: PathBuf) {
+    let out = io::stdout();
+    let another_f = f.clone();
 
-//	let buf = vec![0; 4096];
-//	// Read some data, the buffer is passed by ownership and
-//	// submitted to the kernel. When the operation completes,
-//	// we get the buffer back.
-//	let (res, buf) = file.read_at(buf, 0).await;
-//	let n = res?;
+    // Open the file without blocking
+    let file = File::open(f).await.unwrap();
+    let mut buf = vec![0; 16 * 1_024];
 
-//	// Display the contents
-//	println!("{:?}", &buf[..n]);
+    // Track the current position in the file;
+    let mut pos = 0;
+    let mut out = out.lock();
+    let _whatever = out.write(another_f.to_str().unwrap().as_bytes());
 
-//	Ok(())
-//     })
-// }
+    loop {
+	// Read a chunk
+	let (res, b) = file.read_at(buf, pos).await;
+	let n = res.unwrap();
+
+	if n == 0 {
+	    break;
+	}
+
+	// out.write_all(&b[..n]).unwrap();
+
+	pos += n as u64;
+
+	buf = b;
+    }
+    println!("");
+}
